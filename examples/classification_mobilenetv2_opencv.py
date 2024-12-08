3739dadc36ebea901c781161560c1111622514f3
devemin <52738228+devemin@users.noreply.github.com>
2024-12-08 22:56:12 +0900

    Update classification_mobilenetv2_opencv.py

--- examples/classification_mobilenetv2_opencv.py
+++ examples/classification_mobilenetv2_opencv.py
@@ -175,7 +175,7 @@ def main(model_path, image_path, target_size, crop_size, k):
         #time.sleep(5)
 
         ### Change the comment-out, but now X11Forward not working. ### 
-        #cv2.imshow('camera' , numpy_frame)
+        #cv2.imshow('camera' , frame)
 
 
 
